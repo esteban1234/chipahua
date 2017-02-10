bf8cc5830c4818409582efa92512f43212c3f6cd
esteban1234 <tva_libra@hotmail.com>
2017-02-10 12:43:36 -0600

    index completo

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'vistas/encabezado.php' ?>
 
 <body>
-</body>
+
 
 <article class="contenheader">
 <!-- <p></p> -->
@@ -16,6 +16,77 @@
 			</ul>
 		</div>
 	</header>
+
+	<article class="banner">
+		<p class="tit text-info">LIMPIEZA COMPLETA!</p>
+<p class="subtit">Limpiamos los hogares y centros de trabajo</p>
+	</article>
+
+	<div class="btn-toolbar botonb" role="toolbar">
+  <button type="button" class="btn btn-info btn-lg">
+    <span><i class="fa fa-plus" aria-hidden="true"></i></span> Contáctanos
+  </button>
+</div>
 </article>
 
+
+<article class="contenedor">
+<h1 class="alserv text-info">Algunos de nuestros servicios</h1>
+
+<article class="contenserv">
+<div class="contendivs">
+	<div class="imagen">
+	<article class="icono"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Limpieza</article>
+		<div class="info">
+			<p class="headline">Limpieza</p>
+			<p class="info2">Mantenemos sus instalaciones limpias y en buen estado, convirtiendolas en un espacio agradable que les permita realizar sus actividades como coresponden.</p>
+		</div>
+	</div>
+	
+	<div class="imagen2">
+	<article class="icono2"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Productos de limpieza</article>
+		<div class="info">
+			<p class="headline">Productos de Limpieza</p>
+			<p class="info2">Ofrecemos una gran variedad de productos de limpieza para el hogar o industriales.</p>
+		</div>
+	</div>
+	
+	<div class="imagen3">
+	<article class="icono"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Vigilancia</article>
+		<div class="info">
+			<p class="headline">Vigilancia</p>
+			<p class="info2">Mantenemos sus instalaciones seguras, contamos con guardias de seguridad con experiencia en el área asi como instación de camaras CCTV, mallas electricas, sistemas de alarmas, etc.</p>
+		</div>
+	</div>
+</div>
+
+<!-- <p class="btn btn-info"><a href="">SERVICIOS</a></p> -->
+<div class="btn-toolbar boton" role="toolbar">
+  <button type="button" class="btn btn-info btn-lg">
+    <span><i class="fa fa-plus" aria-hidden="true"></i></span> Servicios
+  </button>
+</div>
+	
+</article>
+
+<article class="bannerindexabajo">
+	<p>Somos la mejor inversión en la vigilancia de sus oficinas.</p>
+	<div class="btn-toolbar boton" role="toolbar">
+  <button type="button" class="btn btn-info btn-lg">
+    <span><i class="fa fa-plus" aria-hidden="true"></i></span> Contátanos
+  </button>
+</div>
+</article>
+	
+</article>
+<footer>
+	<p class="textpie">Derechos de autor 2017© | LIMPIEZA Y VIGILANCIA ESPECIALIZADA CHIPAUA S.A. de C.V.</p>
+	<p class="textpie2">info@chipahua.com</p>
+</footer>
+
+<span class="ir-arriba icon-chevron-up"></span>
+<script src="js/jquery.js"></script>
+<script type="text/javascript" src="js/iconoarriba.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
 </html>
