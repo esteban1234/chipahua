86cd540256b0ba178d796f4f0dfc16da5fb6e272
esteban1234 <tva_libra@hotmail.com>
2017-02-21 09:08:02 -0600

    palabras claves

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -7,8 +7,8 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   	<META NAME="Title" content="LIMPIEZA Y VIGILANCIA ESPECIALIZADA CHIPAUA en ciudad de mérida.">
   	<META NAME="Author" CONTENT="[]"/>
-  	<META name="Description" content="Somos una compañía ubicada en la ciudad de mérida, mérida, dedicados a ofrecer servicios de banquetes en general."/>
-  	<META NAME="Keywords" CONTENT="Bodas en mérida, eventos en mérida, banquetes, organización de banquetes, banquetes en mérida yucatan, banquetes de bodas, coordinadores de bodas, coordinación de bodas, coordinación de eventos, catering, bodas, xv años, graduaciones, bautizos, fiestas, organización de eventos, organización de bodas, renta equipo eventos, renta mobiliario, renta de sillas, renta de mesas, renta salas lounge, renta de toldos, catering bodas, cofee break, servicios para bodas, servicios para eventos, mobiliario para bodas, mobiliario para eventos."/>
+  	<META name="Description" content="Somos una compañía ubicada en la ciudad de mérida, mérida, dedicados a ofrecer servicios de limpieza y vigilancia."/>
+  	<META NAME="Keywords" CONTENT="Limpieza en mérida, seguridad en mérida, Limpieza, organización de Limpieza, Limpieza en mérida yucatan, Limpieza de empresas y hogares, seguridad de empresas y hogares, coordinación de seguridad, empresas y hogares, renta de equipo para seguridad, venta de productos para limpieza."/>
   	<link rel="stylesheet" href="css/bootstrap.css">
   	<link rel="stylesheet" href="css/iconos.min.css">
 		<link rel="stylesheet" href="css/estilos.css">
